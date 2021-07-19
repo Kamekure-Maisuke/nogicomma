c99d59968318c4163fcc5bc213f4a80f7dd888b7
KamekuraMaisuke <taisukeoono350@gmail.com>
2021-07-19 17:03:54 +0900

    update script

--- script/make_current_member.sh
+++ script/make_current_member.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-DATA_FILE="$(dirname $0)/../data/current_member.tsv"
 
- [ -f "$DATA_FILE" ] && rm "$DATA_FILE"
+DATA_DIR="$(dirname $0)/../data"
+NOW=$(date '+%Y%m%d')
+
+rm -rf $DATA_DIR/current_*.tsv
 
 curl -s 'https://ja.wikipedia.org/wiki/%E4%B9%83%E6%9C%A8%E5%9D%8246' |
 sed -n -e '/<span class="mw-headline" id="現メンバー">現メンバー/,/<\/table>/p' |
 grep -E -B 3 '<td>.+(都|県|府|州|道)</td>' |
 sed -r -e 's/<[^>]+>//g' -e '/--/d' |
 awk '{if(NR%4) ORS="\t";else ORS="\n"; print}' |
-awk -F '\t' '{print $1"\t"$2"\t"substr($3,1,10)"\t"$4}' > "$DATA_FILE"
+awk -F '\t' '{print $1"\t"$2"\t"substr($3,1,10)"\t"$4}' > "$DATA_DIR/current_member_$NOW.tsv"
